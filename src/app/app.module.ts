d4d67401fcd3cc3939d7808faebc0a0d43950d1c
Thaysa Santos <thaysamarasantos@gmail.com>
2017-10-05 10:07:26 -0300

    pipes examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { PipesExampleComponent } from './pipes-example/pipes-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltroArrayImpuroPipe,
+    FiltroArrayPipe,
+    PipesExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pipes-example/pipes-example.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes-example',
+  templateUrl: './pipes-example.component.html',
+  styleUrls: ['./pipes-example.component.css']
+})
+export class PipesExampleComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms 2nd ed',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRP'
+  };
+
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string;
+
+  addCurso(valor){
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+  obterCursos(){
+    if(this.livros.length === 0 || this.filtro === undefined || this.filtro.trim() === ''){
+      return this.livros;
+    }
+
+    return this.livros.filter((v) =>{
+        if(v.toLowerCase().indexOf(this.filtro.toLowerCase()) > 0){
+          return true;
+        }
+        return false;
+      });
+  }
+
+  valorAsync = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve('valor asincrono'), 2000)})
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
